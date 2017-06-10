424684afec9fcc5e3dae8c3ab08e535779fc40c2
ElijahButers <Elijahbuters@gmail.com>
2017-06-10 11:14:31 +0300

    Added code to the draw() and added a drawText() method to the RoundedLabel class that create rounded edges 5 pixels and the padding for our rounded label.

--- RoundedLabel.swift
+++ RoundedLabel.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-class RoundedLabel: UIButton {
+class RoundedLabel: UILabel {
 
-    /*
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        super.draw(rect)
+        
+        layer.cornerRadius = 5.0
+        layer.masksToBounds = true
     }
-    */
+    
+    override func drawText(in rect: CGRect) {
+        
+        let newRect = rect.insetBy(dx: 8.0, dy: 8.0)
+        super.drawText(in: newRect)
+    }
+    
 
 }
