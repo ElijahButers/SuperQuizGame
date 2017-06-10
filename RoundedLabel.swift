1a7f11c584679beec2a0b59f57140463a982a1af
ElijahButers <Elijahbuters@gmail.com>
2017-06-10 10:37:33 +0300

    Created a cocoa touch file of UILabel class to project.

---
+++ RoundedLabel.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedLabel.swift
+//  SuperQuizGame
+//
+//  Created by User on 6/10/17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+import UIKit
+
+class RoundedLabel: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
