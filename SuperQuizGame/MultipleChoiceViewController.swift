566b1531710f278a6ffc11436f746f2590b77ce1
ElijahButers <Elijahbuters@gmail.com>
2017-06-19 18:43:47 +0300

    Added code to viewDidLoad() to MultipleChoiceViewController class that creates a white background in the view.

--- SuperQuizGame/MultipleChoiceViewController.swift
+++ SuperQuizGame/MultipleChoiceViewController.swift
@@ -12,6 +12,8 @@ class MultipleChoiceViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
 
     }
 
