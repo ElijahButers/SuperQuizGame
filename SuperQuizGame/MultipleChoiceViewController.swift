1d1541036133026ee6cce10605d885a851ef3c36
ElijahButers <Elijahbuters@gmail.com>
2017-06-21 12:43:36 +0300

    Updated code in MultipleChoiceViewController class where we updated properties from let to var for the content view, question view, question label, question button, answer view, answer buttons, countdown view and progress view constraints and answer rounded buttons.

--- SuperQuizGame/MultipleChoiceViewController.swift
+++ SuperQuizGame/MultipleChoiceViewController.swift
@@ -11,25 +11,25 @@ import UIKit
 class MultipleChoiceViewController: UIViewController {
     
     private let contentView = UIView()
-    private let contentViewConstraints: [NSLayoutConstraint]!
+    private var contentViewConstraints: [NSLayoutConstraint]!
     
     private let questionView = UIView()
-    private let questionViewConstraints: [NSLayoutConstraint]!
+    private var questionViewConstraints: [NSLayoutConstraint]!
     
     private let answerView = UIView()
-    private let answerViewConstraints: [NSLayoutConstraint]!
+    private var answerViewConstraints: [NSLayoutConstraint]!
     
     private let countdownView = UIView()
-    private let countdownViewConstraints: [NSLayoutConstraint]!
+    private var countdownViewConstraints: [NSLayoutConstraint]!
     
     private let questionLabel = RoundedLabel()
-    private let questionLabelConstraints: [NSLayoutConstraint]!
+    private var questionLabelConstraints: [NSLayoutConstraint]!
     private let questionButton = RoundedButton()
-    private let questionButtonConstraints: [NSLayoutConstraint]!
-    private let answerButtons = [RoundedButton]()
-    private let answerButtonsConstraints: [NSLayoutConstraint]!
+    private var questionButtonConstraints: [NSLayoutConstraint]!
+    private var answerButtons = [RoundedButton]()
+    private var answerButtonsConstraints: [NSLayoutConstraint]!
     private let progressView = UIProgressView()
-    private let progressViewConstraints: [NSLayoutConstraint]!
+    private var progressViewConstraints: [NSLayoutConstraint]!
     
     private let backgroundColor = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0)
     private let foregroundColor = UIColor(red: 52/255, green: 73/255, blue: 94/255, alpha: 1.0)
