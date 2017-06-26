4707606e064fbe93de45cc54bdc50c170ac975d3
ElijahButers <Elijahbuters@gmail.com>
2017-06-26 22:11:34 +0300

    Added code to loadMultipleChoiceQuiz() method in the QuizLoader class that downloads the question, correct answer and answers from dictionary in the plist file otherwise throws an error.

--- SuperQuizGame/QuizLoader.swift
+++ SuperQuizGame/QuizLoader.swift
@@ -20,4 +20,21 @@ enum LoaderError: Error {
 
 class QuizLoader {
     
+    public func loadMultipleChoiceQuiz(forQuiz quizName: String) throws -> [MultipleChoiceQuestion] {
+        var questions = [MultipleChoiceQuestion]()
+        if let path = Bundle.main.path(forResource: quizName, ofType: "plist") {
+            if let dict = NSDictionary(contentsOfFile: path) {
+                let tempArray: Array = dict["Questions"]! as! [Dictionary<String, AnyObject>]
+                for dictionary in tempArray {
+                    let questionToAdd = MultipleChoiceQuestion(question: dictionary["Question"] as! String, correctAnswer: dictionary["CorrectAnswer"] as! String, answers: dictionary["Answers"] as! [String])
+                    questions.append(questionToAdd)
+                }
+                return questions
+            } else {
+                throw LoaderError.dictionaryFailed
+            }
+        } else {
+            throw LoaderError.pathFailed
+        }
+    }
 }
