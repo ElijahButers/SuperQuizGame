450e6a9c8b45d75f3ec14ffed7bc9fc68c229c38
ElijahButers <Elijahbuters@gmail.com>
2017-06-28 18:53:39 +0300

    Added a QuizAlertView class, subview of UIView to project.

---
+++ SuperQuizGame/QuizAlertView.swift
@@ -0,0 +1,14 @@
+//
+//  QuizAlertView.swift
+//  SuperQuizGame
+//
+//  Created by User on 6/28/17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+import UIKit
+
+class QuizAlertView: UIView {
+
+
+}
