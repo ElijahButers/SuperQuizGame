53e98a49591de374c59f2c9fee97418eeb8dc09e
ElijahButers <Elijahbuters@gmail.com>
2017-06-29 23:56:18 +0300

    Added code to QuizAlertView class where properties represent alert view, rounded title label, rounded message label and close button.

--- SuperQuizGame/QuizAlertView.swift
+++ SuperQuizGame/QuizAlertView.swift
@@ -14,4 +14,17 @@ class QuizAlertView: UIView {
     private let titleLabel = RoundedLabel()
     private let messageLabel = RoundedLabel()
     let closeButton = UIButton()
+    
+    init(withTitle title: String, andMessage message: String, colors: [UIColor]) {
+        super.init(frame: CGRect.zero)
+        titleLabel.text = title
+        messageLabel.text = message
+        alertView.backgroundColor = colors[0]
+        closeButton.backgroundColor = colors[1]
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
